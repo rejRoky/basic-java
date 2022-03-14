48ae6245ac4a1e490e8a0fe8e7047842a4ed7ceb
rejRoky <rejaul.islam.roky@gmail.com>
2022-03-15 00:27:33 +0600

    Data type.java uploaded

---
+++ dataType.java
@@ -0,0 +1,44 @@
+import java.lang.*;
+
+class dataType
+{
+	public static void main (String arg[])
+	{
+		// Range of Datatype : Byte
+		System.out.println("Byte");
+		System.out.println("Range: "+Byte.MIN_VALUE + " TO " +Byte.MAX_VALUE);
+		System.out.println("Size : "+Byte.BYTES +" byte");
+
+		// Range of Datatype : Short
+		System.out.println("\nShort");
+		System.out.println("Range: "+Short.MIN_VALUE + " TO " +Short.MAX_VALUE);
+		System.out.println("Size : "+Short.BYTES +" byte");
+		
+		// Range of Datatype : Integer
+		System.out.println("\nInteger");
+		System.out.println("Range: "+Integer.MIN_VALUE + " TO " +Integer.MAX_VALUE);
+		System.out.println("Size : "+Integer.BYTES +" byte");
+		
+		// Range of Datatype : Long
+		System.out.println("\nLong");
+		System.out.println("Range: "+Long.MIN_VALUE + " TO " +Long.MAX_VALUE);
+		System.out.println("Size : "+Long.BYTES + " byte");
+		
+		// Range of Datatype : Float
+		System.out.println("\nFloat");
+		System.out.println("Range: "+Float.MIN_VALUE + " TO " +Float.MAX_VALUE);
+		System.out.println("Size : "+Float.BYTES + " byte");
+
+		// Range of Datatype : Double
+		System.out.println("\nDouble");
+		System.out.println("Range: "+Double.MIN_VALUE + " TO " +Double.MAX_VALUE);
+		System.out.println("Size : "+Double.BYTES + " byte");
+
+		// Range of Datatype : Character
+		System.out.println("\nCharacter");
+		System.out.println("Range: "+Character.MIN_VALUE + " TO " +Character.MAX_VALUE);
+		System.out.println("Size : "+Character.BYTES + " byte");
+
+	
+	}
+}
