b72010e88b9c3d2411de1386eeab238392b4dcdf
rejRoky <rejaul.islam.roky@gmail.com>
2022-03-15 14:02:13 +0600

    Operation.java uploaded

---
+++ Oparetion.java
@@ -0,0 +1,73 @@
+import java.lang.*;
+import java.util.Scanner;
+
+import javax.swing.text.Style;
+
+class Oparetion 
+{
+	public static void main (String arg [])
+	{
+		int aa = 10; 
+		int bb = 3;
+
+		float cc = (float) aa/bb;
+		int r = aa%bb;
+
+		System.out.println(cc);
+		System.out.println(r);
+
+		float  m = 10.25f , n = 3.3f; 
+	
+
+		float o = m/n;
+		float p = m%n;
+
+		System.out.println(o);
+		System.out.println(p);
+
+
+		byte b = 10;	////
+		short s = 20;	////	int
+		int i = 40;		////    Type
+		char c =10; 	////
+
+		long l = 323l;
+
+		float f = 32.33f;
+		double d = 325.3665;
+
+
+		double x = f / d;    /// take larger size datatype
+		System.out.println(x);   
+
+		int y = c+i;		/// take larger size datatype
+		System.out.println(y);
+
+
+		System.out.println((10+20)/3);
+
+
+		/// Calculate Area of Triangle 
+
+		float baseOfTriangle ;
+		float HeightOfTriangle;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Base of Triangle: ");
+		baseOfTriangle = sc.nextFloat();
+		System.out.println("Enter Hight of Triangle: ");
+		HeightOfTriangle = sc.nextFloat();
+
+		float areaOfTriangle = (1f/2f)*baseOfTriangle*HeightOfTriangle;
+		System.out.println("Area Of Triangle: "+ areaOfTriangle);
+
+		//-------------------------------------------------------------------------
+
+		
+
+
+
+
+
+	}
+}
